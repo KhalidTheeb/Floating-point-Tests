2068bfa7def0ffd9fa85e72e34285fa4e5e0a2d6
KhalidTheeb <khalidtheeb@gmail.com>
2017-10-04 13:37:31 -0600

    Update exp_bug.c

--- exp_bug.c
+++ exp_bug.c
@@ -5,6 +5,9 @@ Floating-point Bugs in Embedded GNU C Library.
 No. CMU-CS-13-130. CARNEGIE-MELLON UNIV PITTSBURGH PA SCHOOL OF COMPUTER SCIENCE, 2013.
 */
 
+//The result should be in the range between -1 and 1
+//$ gcc exp_bug.c -lm && ./a.out
+
 #include <math.h>
 #include <fenv.h>
 #include <stdio.h>
